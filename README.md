a644dadf161127dd1393d4e820a09cee198c76ce
vastcombat <>
2022-03-28 12:40:45 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# VastCombat.io!
+This is the place where the developers, including you can create this _.io_ game!
+
+Don't worry if you are unsure on how to make this game! Continue reading!
+
+## Root Directory
---
+++ js/config.js
@@ -0,0 +1,19 @@
+const vastcombatConfig = {
+  appName: "VastCombat.io",
+  appVersion: "0.0.0",
+  developers: ["Fun X Time", "EAZY BLACK", "Ibrahim"],
+  artists: ["Fun X Time"],
+  musicArtists: [null],
+  discord: {
+    inviteCode: null,
+    inviteLink: null,
+  },
+  hostname: {
+    primary: "vastcombat.cf",
+    alias: {
+      replit: "repl.vastcombat.cf",
+      netlify: "vastcombat.netlify.app",
+      netlifywithdomain: "vastcombat.cf"
+    }
+  }
+}
