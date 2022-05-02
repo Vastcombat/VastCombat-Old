771e7d97c9364d8b31d3a237171c31a1c3d7dffc
Fun X Time <funxtimecontact@gmail.com>
2022-05-02 16:30:55 +0000

    :(

--- js/eventListeners.js
+++ js/eventListeners.js
@@ -1,7 +1,9 @@
-document.getElementById("playButton").addEventListener(function(){ 
-  OneSignal.push(function() {            
+/* document.getElementById("playButton").addEventListener(function(){ 
+  OneSignal.push(function() {
     OneSignal.sendTag("hasPlayed", "true").then(function(tagsSent) {
       console.log("OneSignal | Tag \"hasPlayed\" has been set to \"true\"!");
     }); 
   });
-}
+} */
+
+// Doesn't work anymore :(
