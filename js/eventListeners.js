a44b3fa2678d45041a5e2cba19eb69744420e2a3
Fun X Time <funxtimecontact@gmail.com>
2022-05-02 16:28:29 +0000

    BETA - Event Listeners

---
+++ js/eventListeners.js
@@ -0,0 +1,7 @@
+document.getElementById("playButton").addEventListener(function(){ 
+  OneSignal.push(function() {            
+    OneSignal.sendTag("hasPlayed", "true").then(function(tagsSent) {
+      console.log("OneSignal | Tag \"hasPlayed\" has been set to \"true\"!");
+    }); 
+  });
+}
